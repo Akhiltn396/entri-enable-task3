03cf373cdb2ed4257fb14ae554e20a9bcf5fc72e
Akhil_T_N <akhiltn396@gmail.com>
2023-12-11 21:52:12 +0530

    second commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const result = document.getElementById("result");
 
 function fetchDataFromExternalAPI() {
   return new Promise((resolve, reject) => {
-    fetch(" https://jsonplaceholder.typicode.com/todos") // Replace 'https://api.example.com/data' with your API endpoint
+    fetch(" https://jsonplaceholder.typicode.com/todos")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -11,27 +11,26 @@ function fetchDataFromExternalAPI() {
         return response.json();
       })
       .then((data) => {
-        resolve(data); // Resolve the promise with the fetched data
+        resolve(data);
         console.log(data);
       })
       .catch((error) => {
-        reject(error); // Reject the promise if there's an error
+        reject(error);
       });
   });
 }
 
-// Fetch data from the external API and display it in an unordered list
 document.getElementById("myButton").addEventListener("click", function () {
   fetchDataFromExternalAPI()
     .then((data) => {
       data.forEach((item) => {
         const listItem = document.createElement("li");
-        listItem.textContent = `Name: ${item.title} Completed: ${item.completed}` ; // Replace with your object properties
+        listItem.textContent = `Name: ${item.title} Completed: ${item.completed}`;
         result.appendChild(listItem);
       });
     })
     .catch((error) => {
       console.error("There was a problem fetching the data:", error);
-      result.innerHTML = "There was a problem fetching the data"
+      result.innerHTML = "There was a problem fetching the data";
     });
 });
